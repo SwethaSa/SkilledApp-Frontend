8d138effdd21cb34d3be65c5aebec2d1d7ad9dbd
Swetha Sakthi <swethasakthi927@gmail.com.com>
2025-05-09 14:48:30 +0530

    all course

--- src/Pages/Dashboard/AllCourses.jsx
+++ src/Pages/Dashboard/AllCourses.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Interface from "../../Components/Interface/Interface";
+import "./AllCourses.css";
 
 function AllCourses() {
   return (
@@ -12,7 +13,13 @@ function AllCourses() {
         ButtonFour="Discussions"
         ButtonFive="All Courses"
         InterfaceImg="./src/assets/mycourse.svg"
-      />
+      >
+        <button className="topic-btn">It</button>
+        <button className="topic-btn">Personal Growth</button>
+        <button className="topic-btn">Finance</button>
+        <button className="topic-btn">Creative Skills</button>
+        <button className="topic-btn">Language</button>
+      </Interface>
     </>
   );
 }
