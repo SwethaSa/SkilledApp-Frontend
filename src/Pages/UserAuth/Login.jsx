a92bb06714c7f3902bf91239c1d069bc1a08f801
Swetha Sakthi <115685787+SwethaSa@users.noreply.github.com>
2025-06-02 11:18:25 +0530

    Update Login.jsx

--- src/Pages/UserAuth/Login.jsx
+++ src/Pages/UserAuth/Login.jsx
@@ -90,7 +90,7 @@ function Login() {
         <div className="login-card">
           <h2 className="login-heading">Hey, welcome back!</h2>
           <div className="login-info-box">
-  <p><strong>For testing mentor portal:</strong></p>
+  <p><strong>For accessing mentor portal:</strong></p>
   <p>Email: <code>swethasakthi1227@gmail.com</code></p>
   <p>Password: <code>Admin@123</code></p>
 </div>
